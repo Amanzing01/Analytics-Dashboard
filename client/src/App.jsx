62c3113e168a5b4d9ed46ef10943b1a269c529e1
AmanAtHumancloud <aman.singh@humancloud.co.in>
2025-07-26 13:28:11 +0530

    First Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import Analytics from "./components/Analytics"
+import Cards from "./components/Cards"
+import Table from "./components/Table"
+
+
+function App() {
+
+
+  return (
+    <>
+    <Cards/>
+    <Analytics/>
+    <Table/>
+    </>
+  )
+}
+
+export default App
